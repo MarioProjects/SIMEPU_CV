fa68082dbf958984003a6ee42384f7b54066dc62
MarioProjects <cocolau6199>
2019-09-21 13:18:47 +0200

    new results

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ epochs=200
 batch_size=32
 img_size=224
 crop_size=224
-model_path="results/"$model"_"$optimizer"_"$img_size"to"$crop_size"_lr"$lr"_NoDa/"
+model_path="results/"$model"_"$optimizer"_"$img_size"to"$crop_size"_lr"$lr"_NoDA/"
 
 CUDA_VISIBLE_DEVICES=0 python train.py --model_name $model --optimizer $optimizer --learning_rate $lr \
                                        --min_learning_rate $min_lr  --batch_size $batch_size --epochs $epochs \
