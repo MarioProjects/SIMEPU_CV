9557ca66a85bf167098dae40e5258fe8ff514364
MarioProjects <cocolau6199>
2019-09-23 09:22:44 +0200

    new results

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo -e "\n---- Start ----\n"
 model="resnet18" #resnet_unet
-optimizer="sgd"
-lr=0.01 # learning_rate
-min_lr=0.00001
+optimizer="adam"
+lr=0.001 # learning_rate
+min_lr=0.0000001
 epochs=200
-batch_size=32
-img_size=224
+batch_size=128
+img_size=256
 crop_size=224
 model_path="results/"$model"_"$optimizer"_"$img_size"to"$crop_size"_lr"$lr"_NoDA/"
 
 CUDA_VISIBLE_DEVICES=0 python train.py --model_name $model --optimizer $optimizer --learning_rate $lr \
                                        --min_learning_rate $min_lr  --batch_size $batch_size --epochs $epochs \
                                        --img_size $img_size --crop_size $crop_size --output_dir $model_path \
-                                       --steps_scheduler
+                                       --steps_scheduler --pretrained --data_augmentation
