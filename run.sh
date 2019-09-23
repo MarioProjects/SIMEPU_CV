111ad9e30ab6462e9e9f11132c04015517d87464
MarioProjects <cocolau6199>
2019-09-23 10:25:48 +0200

    fix weighted loss

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ epochs=200
 batch_size=128
 img_size=256
 crop_size=224
-model_path="results/"$model"_"$optimizer"_"$img_size"to"$crop_size"_lr"$lr"_DA_pretrained/"
+model_path="results/"$model"_"$optimizer"_"$img_size"to"$crop_size"_lr"$lr"_DA_pretrained_weightedLoss/"
 
 CUDA_VISIBLE_DEVICES=0 python train.py --model_name $model --optimizer $optimizer --learning_rate $lr \
                                        --min_learning_rate $min_lr  --batch_size $batch_size --epochs $epochs \
                                        --img_size $img_size --crop_size $crop_size --output_dir $model_path \
-                                       --steps_scheduler --pretrained --data_augmentation
+                                       --steps_scheduler --pretrained --data_augmentation --weighted_loss
