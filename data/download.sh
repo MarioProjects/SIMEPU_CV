e7450e2ca29949200f57940810f6225657b691cf
MarioProjects <maparla@inf.upv.es>
2021-02-02 21:51:14 +0100

    update data

--- data/download.sh
+++ data/download.sh
@@ -2,7 +2,7 @@
 if [ ! -d "data/SIMEPU" ]
 then
     echo "SIMEPU data not found at 'data' directory. Downloading..."
-    curl -O -J https://nextcloud.maparla.duckdns.org/s/GLMQYE6ckcbYDJ2/download
+    curl -O -J https://nextcloud.maparla.duckdns.org/s/yjf6Tn6yN3FEP6T/download
     mkdir -p data
     tar -zxf simepu_ene29_2021_postAllMulti.tar.gz  -C data/
     rm simepu_ene29_2021_postAllMulti.tar.gz
